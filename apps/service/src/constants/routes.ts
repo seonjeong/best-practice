e3c0a0cd8c57207f529e02cad3d31ad065b86c61
theblessedcity <theblessedcity@hanmail.net>
2025-11-17 22:20:52 +0900

    chore(apps/service): /, /login routes 연결 및 빈 page 컴포넌트 생성

--- apps/service/src/constants/routes.ts
+++ apps/service/src/constants/routes.ts
@@ -1,1 +1,4 @@
-export const ROUTES = {};
+export const ROUTES = {
+  HOME: '/',
+  LOGIN: '/login',
+};
--- apps/service/src/routes/routes.tsx
+++ apps/service/src/routes/routes.tsx
@@ -1,10 +1,23 @@
 import type { ReactNode } from 'react';
 
+import { ROUTES } from '@/constants/routes';
+
+import { Login, Home } from '@/pages';
+
 interface Route {
   path: string;
   element: ReactNode;
 }
 
-const routes: Route[] = [];
+const routes: Route[] = [
+  {
+    path: ROUTES.HOME,
+    element: <Home />,
+  },
+  {
+    path: ROUTES.LOGIN,
+    element: <Login />,
+  },
+];
 
 export { routes };
