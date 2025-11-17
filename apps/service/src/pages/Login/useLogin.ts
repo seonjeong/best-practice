fddcc368acf928575703e2ebc579da85956c22bc
theblessedcity <theblessedcity@hanmail.net>
2025-11-17 23:18:34 +0900

    feat(apps/service:login): login submit 처리

--- apps/service/src/pages/Login/useLogin.ts
+++ apps/service/src/pages/Login/useLogin.ts
@@ -2,6 +2,10 @@ import { useForm } from 'react-hook-form';
 
 import { zodResolver } from '@hookform/resolvers/zod';
 
+import { useMutation } from '@tanstack/react-query';
+
+import { postLogin } from '@/apis/generated/api/login/login';
+
 import { loginShema } from './loginSchema';
 import type { LoginData } from './loginSchema';
 
@@ -11,14 +15,29 @@ const defaultValues = {
 };
 
 const useLogin = () => {
-  const { register } = useForm<LoginData>({
+  const { register, handleSubmit } = useForm<LoginData>({
     mode: 'onChange',
     resolver: zodResolver(loginShema),
     defaultValues,
   });
 
+  const mutation = useMutation({
+    mutationFn: async (values: LoginData) => postLogin(values),
+  });
+
+  const onValid = async (values: LoginData) => {
+    try {
+      await mutation.mutate(values);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const onSubmit = handleSubmit(onValid);
+
   return {
     register,
+    onSubmit,
   };
 };
 
