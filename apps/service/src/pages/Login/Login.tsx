e803ea073a1a5b8f9ad8c8fdb68375ce21ba8335
theblessedcity <theblessedcity@hanmail.net>
2025-11-17 23:10:32 +0900

    chore(apps/service): Login 페이지 render html template

--- apps/service/src/pages/Login/Login.tsx
+++ apps/service/src/pages/Login/Login.tsx
@@ -1,5 +1,37 @@
 function Login() {
-  return null;
+  return (
+    <>
+      <form onSubmit={() => {}} noValidate>
+        <input id="email" type="email" autoComplete="email" value="" onChange={() => {}} />
+        <input
+          id="password"
+          type="password"
+          autoComplete="current-password"
+          value=""
+          onChange={() => {}}
+        />
+        <button type="submit">Login</button>
+      </form>
+
+      <>
+        <div
+          style={{
+            display: 'none',
+            position: 'fixed',
+            background: 'rgba(0,0,0,0.5)',
+            color: '#fff',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+          }}
+        >
+          <button onClick={() => {}}>close</button>
+          <p>error message</p>
+        </div>
+      </>
+    </>
+  );
 }
 
 export { Login };
