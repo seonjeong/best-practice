b92a8a10bcd7674c8bcb7919d03d66ee7ff073aa
theblessedcity <theblessedcity@hanmail.net>
2025-11-18 16:13:09 +0900

    refactor(apps/service:convention): import 순서 맞추기

---
+++ apps/service/src/configs/env.ts
@@ -0,0 +1,11 @@
+const ENV = {
+  VITE_APP_NAME: '',
+  VITE_APP_URL: '',
+  VITE_ENV: '',
+  VITE_API_URL: '',
+  VITE_CLIENT_ID: '',
+  VITE_REDIRECT_URI: '',
+  VITE_GA_ID: '',
+} as const;
+
+export { ENV };
