bef6509013888283e248816a87bf3fc0bd17ecf8
theblessedcity <theblessedcity@hanmail.net>
2025-11-20 16:54:04 +0900

    fix(apps/service:orval): orval 에서 사용할 customInstance의 타입 불일치 오류 해결

--- apps/service/src/apis/mutator/custom-instance.ts
+++ apps/service/src/apis/mutator/custom-instance.ts
@@ -1,16 +1,22 @@
 import Axios from 'axios';
-import type { AxiosRequestConfig } from 'axios';
+import type { AxiosRequestConfig, GenericAbortSignal } from 'axios';
 
 export const AXIOS_INSTANCE = Axios.create({});
 
+export type OrvalCompatibleAxiosConfig = Omit<AxiosRequestConfig, 'signal'> & {
+  signal?: AbortSignal | GenericAbortSignal | undefined;
+};
+
 export const customInstance = async <T>(
-  config: AxiosRequestConfig,
+  config: OrvalCompatibleAxiosConfig,
   options?: AxiosRequestConfig,
 ): Promise<T> => {
-  const { data } = await AXIOS_INSTANCE({
+  const axiosConfig = {
     ...config,
     ...options,
-  });
+  } as AxiosRequestConfig;
+
+  const { data } = await AXIOS_INSTANCE(axiosConfig);
 
   return data;
 };
