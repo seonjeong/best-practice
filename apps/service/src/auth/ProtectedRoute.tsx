199c0771063abfbe021d0a735009329689338c5a
theblessedcity <theblessedcity@hanmail.net>
2025-11-21 17:01:20 +0900

    feat(apps/service:ProtectedRoute): 로그인이 안되어 있는 경우 /login 으로 이동 시키는 컴포넌트

---
+++ apps/service/src/auth/ProtectedRoute.tsx
@@ -0,0 +1,28 @@
+import { useEffect } from 'react';
+import type { ReactNode } from 'react';
+
+import { useNavigate } from 'react-router-dom';
+
+import { useAuthContext } from '@/contexts';
+
+type Props = {
+  children: ReactNode;
+};
+
+const ProtectedRoute = ({ children }: Props) => {
+  const navigate = useNavigate();
+
+  const { isAuthenticated } = useAuthContext();
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate('/login');
+    }
+  }, [navigate, isAuthenticated]);
+
+  if (!isAuthenticated) return null;
+
+  return children;
+};
+
+export { ProtectedRoute };
